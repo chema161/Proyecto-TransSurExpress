cb7de1be7d2468d01b4bd3d8948a17894e993e94
chema161 <diaz.gajos24@triana.salesianos.edu>
2026-05-29 20:13:28 +0200

    validaciones añadidas al html y entidades

--- Proyecto_TransSurExpress_JoséManuel_Díaz/src/main/java/com/salesianostriana/dam/proyectotranssurexpressjosemanueldiaz/modelos/Conductor.java
+++ Proyecto_TransSurExpress_JoséManuel_Díaz/src/main/java/com/salesianostriana/dam/proyectotranssurexpressjosemanueldiaz/modelos/Conductor.java
@@ -1,10 +1,8 @@
 package com.salesianostriana.dam.proyectotranssurexpressjosemanueldiaz.modelos;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
@@ -15,9 +13,17 @@ public class Conductor {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres")
     private String nombre;
+
+    @NotNull(message = "Los años de experiencia son obligatorios")
+    @Min(value = 0, message = "La experiencia no puede ser negativa")
+    @Max(value = 50, message = "La experiencia no puede superar los 50 años")
     private Integer experiencia;
 
     @ManyToOne
     private Vehiculo vehiculo;
+    // El vehículo es opcional: un conductor puede estar sin vehículo asignado
 }
